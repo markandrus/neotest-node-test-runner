3614e413de336a4d0167671bc8876070b3a6f2fc
Mark Roberts <mark.roberts@vercel.com>
2025-04-24 22:15:36 +0200

    chore: expand test file matching

--- lua/neotest-node-test-runner/core/discoverer.lua
+++ lua/neotest-node-test-runner/core/discoverer.lua
@@ -8,23 +8,23 @@ local Discoverer = {}
 function Discoverer.discover_positions(path)
 	local query = [[
     ; -- Namespaces --
-    ; Matches: `describe('context')`
+    ; Matches: `describe('context') / suite('context')`
     ((call_expression
-      function: (identifier) @func_name (#eq? @func_name "describe")
+      function: (identifier) @func_name (#any-of? @func_name "describe" "suite")
       arguments: (arguments (string (string_fragment) @namespace.name) (arrow_function))
     )) @namespace.definition
-    ; Matches: `describe.only('context')`
+    ; Matches: `describe.only('context') / suite.only('context')`
     ((call_expression
       function: (member_expression
-        object: (identifier) @func_name (#any-of? @func_name "describe")
+        object: (identifier) @func_name (#any-of? @func_name "describe" "suite")
       )
       arguments: (arguments (string (string_fragment) @namespace.name) (arrow_function))
     )) @namespace.definition
-    ; Matches: `describe.each(['data'])('context')`
+    ; Matches: `describe.each(['data'])('context') / suite.each(['data'])('context')`
     ((call_expression
       function: (call_expression
         function: (member_expression
-          object: (identifier) @func_name (#any-of? @func_name "describe")
+          object: (identifier) @func_name (#any-of? @func_name "describe" "suite")
         )
       )
       arguments: (arguments (string (string_fragment) @namespace.name) (arrow_function))
@@ -71,12 +71,11 @@ function Discoverer.is_test_file(file_path)
 		is_test_file = true
 	end
 
-	for _, x in ipairs({ "spec", "test" }) do
-		for _, ext in ipairs({ "js", "jsx", "coffee", "ts", "tsx" }) do
-			if string.match(file_path, "%." .. x .. "%." .. ext .. "$") then
-				is_test_file = true
-				goto matched_pattern
-			end
+	-- https://nodejs.org/api/test.html#running-tests-from-the-command-line
+	for _, ext in ipairs({ "cjs", "mjs", "js", "cts", "mts", "ts" }) do
+		if string.match(file_path, "%." .. ext .. "$") then
+			is_test_file = true
+			goto matched_pattern
 		end
 	end
 	::matched_pattern::
